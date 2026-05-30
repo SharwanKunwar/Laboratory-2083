807040529bcfa47ac822f890121e5ebe6ee91136
SharwanKunwar <sharwankunwar07@gmail.com>
2026-05-30 10:07:11 +0545

    updated about book

--- JAVA/JAVA/Core java/src/oops/Problem06/Code.java
+++ JAVA/JAVA/Core java/src/oops/Problem06/Code.java
@@ -3,6 +3,7 @@ package oops.Problem06;
 public class Code {
     public static void main(String[] args) {
         DeathNote note = new DeathNote("xyz","He doesn't deserve this life. he killed a child.");
-        note.displayPerson();
+//        note.displayWho();
+        note.aboutBook();
     }
 }
--- JAVA/JAVA/Core java/src/oops/Problem06/DeathNote.java
+++ JAVA/JAVA/Core java/src/oops/Problem06/DeathNote.java
@@ -6,19 +6,25 @@ import java.util.List;
 public class DeathNote {
     private final String bookName = "Death Note";
     private final String pages = "infinity";
-    private final String about = "";
-    private String person;
+    private final String about = "The Death Note is a supernatural notebook with a \nchilling rule: write a victim's name while picturing \ntheir face, and they will die. Discovered by genius \nstudent Light Yagami, it becomes a weapon in his quest \nto create a utopian world — free of criminals — at any \ncost.";
+    private String victim;
     private String deathReason;
 
 
-    DeathNote(String person, String deathReason) {
-        this.person = person;
+    DeathNote(String victim, String deathReason) {
+        this.victim = victim;
         this.deathReason = deathReason;
     }
 
-    protected void displayPerson() {
-        System.out.println("Name : " + person);
+    protected void displayWho() {
+        System.out.println("Name : " + victim);
         System.out.println("\nDeath Reason : \n" + deathReason);
+    }
+
+    protected void aboutBook(){
+        System.out.println("Book Name : " + bookName);
+        System.out.println("Book Pages : " + pages);
+        System.out.println("\nBook About : \n" + about);
     }
 
 
