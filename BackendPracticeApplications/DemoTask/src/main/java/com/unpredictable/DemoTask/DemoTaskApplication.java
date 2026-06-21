6117d96973b6c5dfd142635d4ad309c5ae9bc2ac
SharwanKunwar <sharwankunwar07@gmail.com>
2026-06-21 22:32:23 +0545

    added Dotenv setup

--- BackendPracticeApplications/DemoTask/src/main/java/com/unpredictable/DemoTask/DemoTaskApplication.java
+++ BackendPracticeApplications/DemoTask/src/main/java/com/unpredictable/DemoTask/DemoTaskApplication.java
@@ -1,12 +1,16 @@
 package com.unpredictable.DemoTask;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoTaskApplication {
+	public static void main(String[] args)
+	{
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
 
-	public static void main(String[] args) {
 		SpringApplication.run(DemoTaskApplication.class, args);
 	}
 
