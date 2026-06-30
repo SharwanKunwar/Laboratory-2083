bf25fa961cd227f543d3a6a04ac58aa5c9aa8bb5
SharwanKunwar <sharwankunwar07@gmail.com>
2026-06-30 11:21:32 +0545

    added controller

---
+++ SpringBoot/LoginWith_oAuth2_Google/LoginWithGoogle4/src/main/java/com/example/LoginWithGoogle4/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.LoginWithGoogle4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "Hi there!";
+    }
+}
