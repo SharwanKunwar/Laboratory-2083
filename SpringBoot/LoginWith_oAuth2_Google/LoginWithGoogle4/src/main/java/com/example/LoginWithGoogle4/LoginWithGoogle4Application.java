e04977100d300c50811ea93404dbfadad70979f4
SharwanKunwar <sharwankunwar07@gmail.com>
2026-06-30 10:48:34 +0545

    added 4th practice of oauth2

---
+++ SpringBoot/LoginWith_oAuth2_Google/LoginWithGoogle4/src/main/java/com/example/LoginWithGoogle4/LoginWithGoogle4Application.java
@@ -0,0 +1,13 @@
+package com.example.LoginWithGoogle4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithGoogle4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithGoogle4Application.class, args);
+	}
+
+}
