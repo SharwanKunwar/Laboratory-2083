7d468e8b9ef2bd0b00a2ce2fb8928ddd526ecfdd
SharwanKunwar <sharwankunwar07@gmail.com>
2026-06-30 10:55:41 +0545

    completed Dotenv setup

--- SpringBoot/LoginWith_oAuth2_Google/LoginWithGoogle4/src/main/java/com/example/LoginWithGoogle4/LoginWithGoogle4Application.java
+++ SpringBoot/LoginWith_oAuth2_Google/LoginWithGoogle4/src/main/java/com/example/LoginWithGoogle4/LoginWithGoogle4Application.java
@@ -1,12 +1,17 @@
 package com.example.LoginWithGoogle4;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoginWithGoogle4Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(LoginWithGoogle4Application.class, args);
 	}
 
