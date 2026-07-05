772b1337db134e0c16d2262b3fc14ad95f5f6a36
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-05 13:47:32 +0545

    added DeathNote backend for practice auth

---
+++ PracticeAllBackendApplicationIdea/DeathNoteBackend/src/test/java/com/unpredictable/practice/DeathNoteBackend/DeathNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unpredictable.practice.DeathNoteBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeathNoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
