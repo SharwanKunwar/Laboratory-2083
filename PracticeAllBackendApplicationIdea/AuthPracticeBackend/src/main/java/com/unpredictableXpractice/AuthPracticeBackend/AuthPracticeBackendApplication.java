73f624b78771795a2d95dc4dcbcc8c9c4d0474d4
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-08 13:35:50 +0545

    added another practice project for auth

---
+++ PracticeAllBackendApplicationIdea/AuthPracticeBackend/src/main/java/com/unpredictableXpractice/AuthPracticeBackend/AuthPracticeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unpredictableXpractice.AuthPracticeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPracticeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPracticeBackendApplication.class, args);
+	}
+
+}
