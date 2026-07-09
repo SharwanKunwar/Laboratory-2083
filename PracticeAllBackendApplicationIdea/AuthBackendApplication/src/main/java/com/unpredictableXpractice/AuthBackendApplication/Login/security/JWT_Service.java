13cdf766d1d06dfdf95496007330e31e0de6f715
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-09 21:42:27 +0545

    updated auth practice backend with login architecture, database connectivity, Dotenv setup etc.

--- PracticeAllBackendApplicationIdea/AuthBackendApplication/src/main/java/com/unpredictableXpractice/AuthBackendApplication/Login/security/JWT_Service.java
+++ PracticeAllBackendApplicationIdea/AuthBackendApplication/src/main/java/com/unpredictableXpractice/AuthBackendApplication/Login/security/JWT_Service.java
@@ -9,18 +9,5 @@ import javax.crypto.SecretKey;
 @Service
 @RequiredArgsConstructor
 public class JWT_Service {
-    private final SecretKey key;
-    private final long accessTtlSeconds;
-    private final long refreshTtlSeconds;
-    private final String issuer;
 
-    public JWT_Service(
-            @Value("${security.jwt.secret}") String secret,
-            @Value("${security.jwt.access-ttl-seconds}") long accessTtlSeconds,
-            @Value("${security.jwt.refresh-ttl-seconds}") long refreshTtlSeconds,
-            @Value("${security.jwt.issuer}") String issuer) {
-        this.accessTtlSeconds = accessTtlSeconds;
-        this.refreshTtlSeconds = refreshTtlSeconds;
-        this.issuer = issuer;
-    }
 }
