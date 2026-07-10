1a18c015d3b2fad6ace1178c73e9a86e4c001c35
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-10 11:00:59 +0545

    updated security -> JWT_Service

--- PracticeAllBackendApplicationIdea/AuthBackendApplication/src/main/java/com/unpredictableXpractice/AuthBackendApplication/Login/security/JWT_Service.java
+++ PracticeAllBackendApplicationIdea/AuthBackendApplication/src/main/java/com/unpredictableXpractice/AuthBackendApplication/Login/security/JWT_Service.java
@@ -1,10 +1,19 @@
 package com.unpredictableXpractice.AuthBackendApplication.Login.security;
 
+import com.unpredictableXpractice.AuthBackendApplication.Login.enitites.Role;
+import com.unpredictableXpractice.AuthBackendApplication.Login.enitites.User;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class JWT_Service {
@@ -14,6 +23,7 @@ public class JWT_Service {
     private final long refreshTtlSeconds;
     private final String issuer;
 
+    //Constructor
     public JWT_Service(
             @Value("${security.jwt.secret}") String secret,
             @Value("${security.jwt.access-ttl-seconds}") long accessTtlSeconds,
@@ -32,7 +42,70 @@ public class JWT_Service {
 
     }
 
-    //generate token:
+    //For generate access token:
+    public String generateToken(User  user)
+    {
+        Instant now = Instant.now();
+        List<String> roles =user.getRoles() == null ? List.of() : user.getRoles().stream().map(Role::getName).collect(Collectors.toList());
+
+        return Jwts.builder()
+                .setId(UUID.randomUUID().toString())
+                .setSubject(user.getId().toString())
+                .setIssuer(issuer)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plusSeconds(accessTtlSeconds)))
+                .addClaims(Map.of(
+                        "email", user.getEmail(),
+                        "roles", roles,
+                        "typ", "access"
+                )).signWith(secretKey, SignatureAlgorithm.HS512).compact();
+    }
+
+    //For generate refresh token:
+    public String generateToken(User  user, String jti)
+    {
+        Instant now = Instant.now();
+        List<String> roles =user.getRoles() == null ? List.of() : user.getRoles().stream().map(Role::getName).collect(Collectors.toList());
+
+        return Jwts.builder()
+                .id(jti)
+                .subject(user.getId().toString())
+                .issuer(issuer)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusSeconds(accessTtlSeconds)))
+                .claim("typ", "refresh").signWith(secretKey, SignatureAlgorithm.HS512).compact();
+    }
+
+    //Parse the token
+    public Jws<Claims> parse(String token)
+    {
+        try{
+            return Jwts.parser().verifyWith(secretKey).build().parseClaimsJws(token);
+        }catch (JwtException e){
+            throw e;
+        }
+
+    }
+
+    // what is it
+    public boolean isAccessToken(String token)
+    {
+        Claims c = parse(token).getPayload();
+        return "refresh".equals(c.get("typ"));
+    }
+
+    //get user id
+    public UUID getUserId(String token)
+    {
+        Claims c = parse(token).getPayload();
+        return UUID.fromString(c.getSubject());
+    }
+
+    // token id
+    public String getJti(String token)
+    {
+        return parse(token).getPayload().getId();
+    }
 
 
 
