193dc1b6ae9d4e38d7826916c5750459ac7f9046
SharwanKunwar <sharwankunwar07@gmail.com>
2026-08-16 20:50:49 +0545

    expirement failed with ai now my turn

---
+++ Expirements/QuotemousAi_01/src/test/java/com/unpredictableXshare/Quotemous/QuotemousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unpredictableXshare.Quotemous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotemousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
