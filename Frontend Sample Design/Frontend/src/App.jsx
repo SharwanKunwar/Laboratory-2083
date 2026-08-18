2093aa9a94458ba931b9616e76516be4f99406e8
SharwanKunwar <sharwankunwar07@gmail.com>
2026-08-18 22:44:48 +0545

    added frontend sample design folder and updated navbar with cool idea

--- Frontend Sample Design/Frontend/src/App.jsx
+++ Frontend Sample Design/Frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="w-full min-h-[2000px] bg-white/30 backdrop-blur-sm mb-5 shadow-sm border-t-0 border border-gray-50/30 rounded-b-lg relative ">
         <Navbar />
         <div className=" w-full h-screen  flex justify-center items-center">
-          quotes are apper here
+          box
         </div>
       </div>
     </div>
