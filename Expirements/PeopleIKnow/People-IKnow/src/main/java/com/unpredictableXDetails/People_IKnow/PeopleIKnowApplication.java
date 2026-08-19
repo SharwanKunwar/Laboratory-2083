6ccf91cfb656f59cb042f5062eadf219cdafe58a
SharwanKunwar <sharwankunwar07@gmail.com>
2026-08-19 17:48:44 +0545

    added a backend practice project where i practice crud operations

---
+++ Expirements/PeopleIKnow/People-IKnow/src/main/java/com/unpredictableXDetails/People_IKnow/PeopleIKnowApplication.java
@@ -0,0 +1,13 @@
+package com.unpredictableXDetails.People_IKnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleIKnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleIKnowApplication.class, args);
+	}
+
+}
---
+++ Expirements/PeopleIKnow/People-IKnow/src/test/java/com/unpredictableXDetails/People_IKnow/PeopleIKnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unpredictableXDetails.People_IKnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleIKnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
